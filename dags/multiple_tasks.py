0466bfae3db32f17f59dbc79c066543f69c5d8ad
Paul Behrisch <me@paulb.im>
2023-07-27 17:33:38 -0400

    multiple_tasks.py

---
+++ dags/multiple_tasks.py
@@ -0,0 +1,41 @@
+import pendulum
+
+from airflow.decorators import dag, task
+
+@dag(
+    schedule=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["example"],
+)
+
+def run_example_taskflow():
+    @task()
+    def getPersonDetails():
+        return {
+            "firstName": "Random",
+            "surName": "Guy"
+        }
+        
+    @task
+    def getLocationDetails():
+        return {
+            "country": "Brazil",
+            "city": "Sao Paulo"
+        }
+
+    @task()
+    def showProfile(firstName, surName, country, city):
+        print(f"{firstName} {surName} lives in {city}, {country}")
+
+   person = getPersonDetails()
+   loc = getLocationDetails()
+   showProfile(
+       person["firstName"]
+       person["surName"],
+       loc["country"],
+       loc["city"]
+   )
+
+ 
+run_example_taskflow()
